a9af7e3479391fbb9d341ff2fb0702a7dd22c0b6
Faysal Ahmed <tfoysalahmed@gmail.com>
2026-01-12 22:38:21 +0600

    added proxy

--- src/proxy.js
+++ src/proxy.js
@@ -3,9 +3,11 @@ import { NextResponse } from 'next/server'
 // This function can be marked `async` if using `await` inside
 export function proxy(request) {
     const isLoggedin = request.cookies.get("auth")?.value == "true";
+
     if (!isLoggedin) {
         return NextResponse.redirect(new URL('/login', request.url))
     }
+
     return NextResponse.next();
 }
 
